2b72dd2ca064753e436ba10369c85a95ac1d1d0a
Phillip Ball <pball@mail.greenriver.edu>
2021-04-27 12:26:49 -0400

    Added breakfast route

--- index.php
+++ index.php
@@ -21,5 +21,11 @@ $f3->route('GET /', function (){
     echo $view->render('views/home.html');
 });
 
+$f3->route('GET /breakfast', function (){
+    // display the home page
+    $view = new Template();
+    echo $view->render('views/breakfast.html');
+});
+
 // run Fat-Free
 $f3->run();
