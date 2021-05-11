caa7aaee404dcac6a57b23134c4778bfc78f2dde
Phillip Ball <pball@mail.greenriver.edu>
2021-05-11 12:07:20 -0400

    I had forgotten to commit last time... So, here's a commit before working again on 05/11/2021

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+// start a session
+session_start();
+
 // require autoload file
 require_once('vendor/autoload.php');
 
@@ -31,6 +34,40 @@ $f3->route('GET /lunch', function (){
     // display the lunch page
     $view = new Template();
     echo $view->render('views/lunch.html');
+});
+
+$f3->route('GET|POST /order1', function (){
+    // if the form has been submitted, add data to session
+    // and send user to next form
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $_SESSION['food'] = $_POST['food'];
+        $_SESSION['meal'] = $_POST['meal'];
+        header('location: order2');
+    }
+
+    // display the first order form
+    $view = new Template();
+    echo $view->render('views/orderForm1.html');
+});
+
+$f3->route('GET|POST /order2', function (){
+    // if the form has been submitted, add data to session
+    // and send user to summary
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        // data validation goes here
+        $_SESSION['conds'] = implode(", ", $_POST['conds']);
+        header('location: summary');
+    }
+
+    // display the second order form
+    $view = new Template();
+    echo $view->render('views/orderForm2.html');
+});
+
+$f3->route('GET /summary', function (){
+    // display the order summary
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 
 // run Fat-Free
